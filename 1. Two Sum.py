2b2de51f78782eae2546399f1b24430a6d5500b4
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2023-01-17 16:38:01 -0500

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,43 @@
+### Classic two sum and classic brute force search solution.
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                two_sum = nums[i] + nums[j]
+                if j > i and two_sum == target:
+                    return i, j
+### Hashmap 2
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return i, hashmap[complement]
+            
+### Hashmap 2
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return i, hashmap[complement]
+            hashmap[nums[i]] = i
