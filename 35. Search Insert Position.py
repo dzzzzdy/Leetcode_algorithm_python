26c71a87bf91e451f90003f12eeca5b12aec237a
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2023-01-18 18:20:59 -0500

    Create 35. Search Insert Position.py

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,19 @@
+# binary search, renew left, right pointers to mid + 1, mid - 1 instead of mid
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right)//2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+        return left
